21ec655de92f50f1c10649a8f7337ac1319f668a
Suraj Chandrasekhar <surajchandrasekhar@D-69-91-149-164.dhcp4.washington.edu>
2016-10-18 15:18:41 -0700

    complete exercise

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -9,17 +9,25 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
+library(dplyr)
+cars<- vehicles
 
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+acura.2015 <- filter(cars, make=="Acura", year==2015)
+acura.2015.max <- filter(acura.2015,hwy == max(hwy))
+acura.2015.max.model <- select(acura.2015.max, model)
+acura.2015.max
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
-
+acura.2015.nest<-select(filter(filter(cars, make=="Acura",year==2015), hwy==max(hwy)), model) 
 
 # Which Accura model has the best hwy MPG in 2015? (pipe operator)
+acura.2015.chain <- filter(cars, make=='Acura', year==2015) %>% filter(hwy == max(hwy)) %>% select(model)
 
-
+acura.2015.max.model
+acura.2015.nest
+acura.2015.chain
 ### Bonus ###
 
 # Write 3 functions, one for each approach.  Then, 
